6a0bcd2f9401ff856af4adcd1283d85c53d38fea
Coderhehe <77240747+Coderhehe@users.noreply.github.com>
2021-05-22 11:26:01 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -21,14 +21,14 @@ document.getElementById("result").innerHTML="<img id='ci' src="+data_uri+">"
 
 console.log("ml5 version",ml5.version);
 
-classifier=ml5.imageClassify("MobileNet",modelLoaded);
+classifier=ml5.imageClassifier("MobileNet",modelLoaded);
 
 function modelLoaded() {
     console.log("Model Loaded!");
 }
 function check(params) {
     img=document.getElementById("ci");
-    classifier.Classify(img,gotResult);
+    classifier.classify(img,gotResult);
 }
 function gotResult(error,result) {
     if (error) {
